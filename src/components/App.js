8800f7096f90d3d37ac1ddb3107c2e3e219e5cf6
BIJOYROY42 <bijoyandstrings@outlook.com>
2025-04-05 12:33:22 -0600

    commit6

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,11 @@
 import React from 'react';
-import './styles/App.css';
+import '../styles/App.css';
 
 function App() {
     return (
         <div className="App">
             <h1>Bijoy Roy</h1>
-            <h2>Course: Enterprise Computing </h2>
+            <h2>Course: Enterprise Computing</h2>
         </div>
     );
 }
